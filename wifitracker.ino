eea9ae80344bdc21e5840cdf9e7bd4694cf4a15a
Bertrik Sikken <bertrik@sikken.nl>
2016-02-27 01:40:34 +0100

    Log ESP MAC address on startup

--- wifitracker.ino
+++ wifitracker.ino
@@ -144,6 +144,11 @@ void setup()
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
     
+    uint8_t mac[6];
+    WiFi.macAddress(mac);
+    print("ESP MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]); 
+    
     SPIFFS.begin();
 }
 
