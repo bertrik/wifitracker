ac09e20cae78c2f110b40379caeddfc3d55b20f2
Bertrik Sikken <bertrik@sikken.nl>
2016-03-26 08:53:28 +0100

    Implement a test function to submit directly to google location services (need key)

--- wifitracker.ino
+++ wifitracker.ino
@@ -20,7 +20,7 @@ static RtcDS3231 rtc;
 static void print(const char *fmt, ...)
 {
     // format it
-    char buf[128];
+    char buf[256];
     va_list args;
     va_start (args, fmt);
     vsnprintf(buf, sizeof(buf), fmt, args);
@@ -114,12 +114,59 @@ static int do_cat(int argc, char *argv[])
     return 0;
 }
 
+char big[2048];
+
+static HTTPClient client;  
+
+static int submit_to_google(int n)
+{
+    char line[128];
+    
+    sprintf(line, "{\"wifiAccessPoints\": [\n");
+    strcpy(big, line);
+    for (int i = 0; i < n; i++) {
+        uint8_t *mac = WiFi.BSSID(i);
+        sprintf(line, "{\"macAddress\":\"%02X:%02X:%02X:%02X:%02X:%02X\",\"signalStrength\":%d}",
+            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], WiFi.RSSI(i));
+        strcat(big, line);
+        if (i < (n - 1)) {
+            strcat(big, ",");
+        }
+        strcat(big, "\n");
+        Serial.println(line);
+    }
+    strcat(big, "]}\n");
+//    Serial.print(big);
+    
+    const char *url = "https://www.googleapis.com/geolocation/v1/geolocate?key=<your api key here>";
+    
+    int size = strlen(big);
+    print("Sending POST request (%d bytes)...", size);
+
+    client.begin(url);
+    client.addHeader("Content-Type", "application/json");
+    char header[16];
+    sprintf(header, "%d", size);
+    client.addHeader("Content-Length", header);
+    int res = client.POST((uint8_t *)big, size);
+    print("code %d\n", res);
+    if (res == HTTP_CODE_OK) {
+        Serial.println(client.getString());
+    }
+    client.end();
+    
+    return res;
+}
 
 static int do_scan(int argc, char *argv[])
 {
     // scan
+    print("Scanning ... ");
     int n = WiFi.scanNetworks();
-    print("Found %d networks:\n", n);
+    print("found %d networks:\n", n);
+    if ((argc > 1) && (strcmp(argv[1], "google") == 0)) {
+        return submit_to_google(n);
+    }
 
     // open a file with the date as name
     RtcDateTime dt = rtc.GetDateTime();
@@ -132,13 +179,12 @@ static int do_scan(int argc, char *argv[])
     }
 
     char line[256];
-    f.print("{\"deviceid\":\"112233445566\",");
-
-    f.print("\"datetime\":");
-    sprintf(line, "%04d-%02d-%02d %02d:%02d:%02d", dt.Year(), dt.Month(), dt.Day(), dt.Hour(), dt.Minute(), dt.Second());
-    f.print("\"");
+    sprintf(line, "{\"deviceid\":\"%08X\",", ESP.getChipId());
     f.print(line);
-    f.print("\",");
+
+    sprintf(line, "\"datetime\":\"%04d-%02d-%02d %02d:%02d:%02d\"", 
+        dt.Year(), dt.Month(), dt.Day(), dt.Hour(), dt.Minute(), dt.Second());
+    f.print(line);
 
     f.print("\"scan\":[");
     for (int i = 0; i < n; i++) {
@@ -331,7 +377,7 @@ static int do_sleep(int argc, char *argv[])
     if (argc == 2) {
         long t = atoi(argv[1]);
         print("Sleeping for %d ms...\n", t);
-        ESP.deepSleep(1000L * t, WAKE_RF_DISABLED);
+        ESP.deepSleep(1000L * t, WAKE_RF_DEFAULT);
         print("Woke up!\n");
     }
     return 0;
@@ -356,7 +402,7 @@ static int do_http(int argc, char *argv[])
     client.begin(url);
     int res = client.sendRequest(cmd, (uint8_t *)payload, strlen(payload));
     if (res == HTTP_CODE_OK) {
-        print("response: %s\n", client.getString().c_str());
+        Serial.println(client.getString());
     }
     client.end();
     
